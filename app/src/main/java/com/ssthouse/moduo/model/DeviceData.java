164f06e12b23733af44ab0d71e9f43c80e34f46b
ssthouse <1194368995@qq.com>
2015-12-21 20:40:19 +0800

    更新设备数据点 更新配置文件

--- app/src/main/java/com/ssthouse/moduo/model/DeviceData.java
+++ app/src/main/java/com/ssthouse/moduo/model/DeviceData.java
@@ -13,10 +13,38 @@ public class DeviceData implements Serializable {
      * 数据的常量key
      */
     public interface DeviceCons {
-        //温度
+        //当前温度
         String KEY_TEMPERATURE = "temperature";
-        //湿度
+        //当前湿度
         String KEY_HUMIDITY = "humidity";
+        //当前亮度
+        String KEY_LUMINANCE = "luminance";
+        //视频开关
+        String KEY_VIDEO = "video";
+        //音频开关
+        String KEY_AUDIO = "audio";
+        //电量
+        String KEY_POWER = "power";
+        //硬件版本
+        String KEY_HW_VERSION = "hw_version";
+        //软件版本
+        String KEY_SW_VERSION = "sw_version";
+        //x轴 头部
+        String KEY_X_HEAD = "x_head";
+        //y轴 头部
+        String KEY_Y_HEAD = "y_head";
+        //z轴 头部
+        String KEY_Z_HEAD = "z_head";
+        //x轴 身体
+        String KEY_X_BODY = "x_body";
+        //y轴 身体
+        String KEY_Y_BODY = "y_body";
+        //z轴 身体
+        String KEY_Z_BODY = "z_body";
+        //扩展类型 命令
+        String KEY_CTRL_CMD = "ctrl_cmd";
+        //扩展类型 数据
+        String KEY_CTRL_DATA = "ctrl_data";
         //cmd类型
         String KEY_CMD = "cmd";
     }
