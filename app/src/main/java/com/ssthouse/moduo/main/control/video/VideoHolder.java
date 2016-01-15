f1706a5fe36c576c1315dcbc8c92ee385c6970a2
ssthouse <1194368995@qq.com>
2016-01-15 22:26:39 +0800

    成功简化videoFragment代码

--- app/src/main/java/com/ssthouse/moduo/main/control/video/VideoHolder.java
+++ app/src/main/java/com/ssthouse/moduo/main/control/video/VideoHolder.java
@@ -166,5 +166,6 @@ public class VideoHolder {
 
     public void stop() {
         media.setMediaStreamStateCallback(null);
+        stopWatch();
     }
 }
--- app/src/main/java/com/ssthouse/moduo/main/view/fragment/VideoFragment.java
+++ app/src/main/java/com/ssthouse/moduo/main/view/fragment/VideoFragment.java
@@ -1,6 +1,5 @@
 package com.ssthouse.moduo.main.view.fragment;
 
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,18 +9,9 @@ import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 import com.afollestad.materialdialogs.MaterialDialog;
-import com.ichano.rvs.viewer.Media;
-import com.ichano.rvs.viewer.Viewer;
-import com.ichano.rvs.viewer.bean.MediaDataDesc;
-import com.ichano.rvs.viewer.callback.MediaStreamStateCallback;
-import com.ichano.rvs.viewer.codec.AudioType;
-import com.ichano.rvs.viewer.constant.MediaStreamState;
-import com.ichano.rvs.viewer.render.GLViewYuvRender;
 import com.ssthouse.moduo.R;
-import com.ssthouse.moduo.main.control.video.AudioHandler;
+import com.ssthouse.moduo.main.control.video.VideoHolder;
 import com.ssthouse.moduo.main.view.activity.VideoActivity;
-
-import timber.log.Timber;
 
 /**
  * 视频通话fragment
@@ -32,30 +22,12 @@ public class VideoFragment extends Fragment {
     private MaterialDialog waitDialog;
 
     /**
-     * SDK控制类
-     */
-    private Viewer viewer;
-    private Media media;
-
-    /**
      * 视频承接控件
      */
     private RelativeLayout surfaceViewLayout;
 
-    private long liveStreamId;//播放流id
-    private long decoderId; //解码器id
+    private VideoHolder videoHolder;
     private long streamerCid;// 要观看的采集端cid
-
-    /**
-     * 视频播放控件
-     */
-    private GLSurfaceView glSurfaceView;
-    private GLViewYuvRender myRenderer;
-
-    /**
-     * 播放控制类
-     */
-    private AudioHandler audioHandler;
 
     /**
      * 获取fragment实例
@@ -81,117 +53,15 @@ public class VideoFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //初始化Video
-        initVideo();
 
         //初始化view
         View rootView = inflater.inflate(R.layout.fragment_video_display, container, false);
-        initView(rootView);
-        return rootView;
-    }
-
-    private void initVideo() {
-        viewer = Viewer.getViewer();
-        media = viewer.getMedia();
-    }
-
-    private void initView(View rootView) {
         surfaceViewLayout = (RelativeLayout) rootView.findViewById(R.id.id_rl_container);
 
-        //初始化播放控件
-        glSurfaceView = new GLSurfaceView(getContext());
-        glSurfaceView.setEGLContextClientVersion(2);
-        myRenderer = new GLViewYuvRender();
-        glSurfaceView.setRenderer(myRenderer);
+        //初始化视频播放类
+        videoHolder = new VideoHolder(getContext(), surfaceViewLayout, streamerCid);
 
-        waitDialog = new MaterialDialog.Builder(getContext())
-                .customView(R.layout.dialog_wait, true)
-                .autoDismiss(false)
-                .build();
-    }
-
-    /**
-     * 解码器回调器
-     */
-    private GLViewYuvRender.RenderYUVFrame renderYUVFrame = new GLViewYuvRender.RenderYUVFrame() {
-        @Override
-        public void onRenderData(byte[] bytes, byte[] bytes1, byte[] bytes2) {
-            media.getVideoDecodedData(liveStreamId, decoderId, bytes, bytes1, bytes2);
-        }
-    };
-
-    /**
-     * 媒体流的回调
-     */
-    private MediaStreamStateCallback mediaStreamStateCallback = new MediaStreamStateCallback() {
-        @Override
-        public void onMediaStreamState(long streamId, MediaStreamState mediaStreamState) {
-            Timber.e("streamId :" + streamId + ",state:" + mediaStreamState.intValue());
-            //隐藏dialog
-            waitDialog.dismiss();
-            //监测链接状态
-            if (mediaStreamState == MediaStreamState.CREATED) {
-                MediaDataDesc desc = media.getStreamDesc(liveStreamId);
-                if (desc == null) {
-                    Timber.e("get media desc error!");
-                    return;
-                }
-                Timber.e("video :" + desc.getVideoType().toString() + ","
-                        + desc.getVideoWidth() + "," + desc.getVideoHeight());
-                Timber.e("audio :" + desc.getAudioType().toString() + ","
-                        + desc.getSampRate());
-                // 根据对端的音视频格式进行编码器初始化，不使用sdk内置h264解码器可以不用关心
-                decoderId = media.initAVDecoder(desc.getAudioType(),
-                        desc.getSampRate());
-                myRenderer.setVideoDimension(desc.getVideoWidth(),
-                        desc.getVideoHeight());
-                myRenderer.setYuvDataRender(renderYUVFrame);
-                if (desc.getAudioType() != AudioType.INVALID) {
-                    audioHandler = new AudioHandler(desc.getSampRate(),
-                            desc.getChannel(), liveStreamId, decoderId, media, streamerCid);
-                    audioHandler.startAudioWorking();
-                }
-            } else {
-                stopWatch();
-            }
-        }
-    };
-
-    //开始实时视频
-    private void startLiveVideo() {
-        surfaceViewLayout.addView(glSurfaceView);
-        liveStreamId = media.openLiveStream(streamerCid, 0, 0, 0);// 测试打开实时视频流
-        Timber.e("liveStreamId :" + liveStreamId + "   cid" + streamerCid);
-        //初始显示等待dialog
-        waitDialog.show();
-    }
-
-    //停止音视频观看
-    private void stopWatch() {
-        if (audioHandler != null) {
-            audioHandler.releaseAudio();// stop audio play and record
-            audioHandler = null;
-        }
-        surfaceViewLayout.removeView(glSurfaceView);// stop video render
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        if (decoderId != 0) {
-            media.destoryAVDecoder(decoderId);// 销毁解码器
-            decoderId = 0;
-        }
-        if (liveStreamId != 0) {
-            media.closeStream(liveStreamId);// 关闭实时流
-            liveStreamId = 0;
-        }
-    }
-
-    //测试自定义命令
-    public void sendCmd(View view) {
-        boolean ret = viewer.getCommand().sendCustomData(streamerCid, "test".getBytes());
-        Timber.e("send cmd ret:" + ret);
+        return rootView;
     }
 
     /*
@@ -200,19 +70,18 @@ public class VideoFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        startLiveVideo();
+        videoHolder.startLiveVideo();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        media.setMediaStreamStateCallback(mediaStreamStateCallback);
+        videoHolder.resume();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        media.setMediaStreamStateCallback(null);
-        stopWatch();
+        videoHolder.stop();
     }
 }
