77770da0b0ef2c8570c05813dc79f1c008f90172
ssthouse <1194368995@qq.com>
2016-01-16 22:51:24 +0800

    打算把图形锁当一个module导入...先提交一次...怕出错

---
+++ app/src/main/java/com/ssthouse/moduo/main/view/activity/account/GestureLockActivity.java
@@ -0,0 +1,22 @@
+package com.ssthouse.moduo.main.view.activity.account;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ssthouse.moduo.R;
+
+/**
+ * 手势密码
+ * Created by ssthouse on 2016/1/16.
+ */
+public class GestureLockActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gesture_lock);
+    }
+}
