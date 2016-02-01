33c142c0249c6f5db6c28cd3af781d06ba1f0b84
ssthouse <1194368995@qq.com>
2016-02-01 18:01:09 +0800

    add: 讯飞sdk集成, 初始化

--- app/src/main/java/com/ssthouse/moduo/App.java
+++ app/src/main/java/com/ssthouse/moduo/App.java
@@ -6,6 +6,8 @@ import com.activeandroid.ActiveAndroid;
 import com.avos.avoscloud.AVOSCloud;
 import com.baidu.android.pushservice.PushConstants;
 import com.baidu.android.pushservice.PushManager;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechUtility;
 import com.ssthouse.moduo.main.model.cons.Constant;
 import com.ssthouse.moduo.main.control.util.AssertsUtils;
 import com.ssthouse.moduo.main.control.util.FileUtil;
@@ -26,6 +28,8 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        //讯飞语音
+        SpeechUtility.createUtility(this, SpeechConstant.APPID +"=56a6efef");
         //初始化 log
         Timber.plant(new Timber.DebugTree());
         //activeAndroid数据库
@@ -38,12 +42,10 @@ public class App extends Application {
         //百度推送
         PushManager.startWork(getApplicationContext(), PushConstants.LOGIN_TYPE_API_KEY,
                 "5cbIdRlHlm10M1IvSAfesaDM");
-        // 初始化sdk,传入appId,登录机智云官方网站查看产品信息获得 AppID
+        // 初始化机智云sdk
         XPGWifiSDK.sharedInstance().startWithAppID(this, Constant.SettingSdkCons.APP_ID);
-        // 设定日志打印级别,日志保存文件名，是否在后台打印数据.
         XPGWifiSDK.sharedInstance().setLogLevel(Constant.SettingSdkCons.LOG_LEVEL,
                 Constant.SettingSdkCons.LOG_FILE_NAME, Constant.isDebug);
-        //复制xpg配置文件
         try {
             //复制assert文件夹中的json文件到设备安装目录。json文件是解析数据点必备的文件
             AssertsUtils.copyAllAssertToCacheFolder(this.getApplicationContext());
