813f02d63d17f260cadfe0c9744f8c38e9c138d4
ssthouse <1194368995@qq.com>
2016-02-02 21:56:37 +0800

    魅族还是不行......怎么回事??

--- app/src/main/java/com/ssthouse/moduo/App.java
+++ app/src/main/java/com/ssthouse/moduo/App.java
@@ -6,13 +6,15 @@ import com.activeandroid.ActiveAndroid;
 import com.avos.avoscloud.AVOSCloud;
 import com.iflytek.cloud.SpeechConstant;
 import com.iflytek.cloud.SpeechUtility;
+import com.ssthouse.moduo.main.control.util.AssertsUtils;
 import com.ssthouse.moduo.main.control.util.FileUtil;
 import com.ssthouse.moduo.main.model.cons.Constant;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.update.UmengUpdateAgent;
 import com.xtremeprog.xpgconnect.XPGWifiSDK;
 
-import cn.jpush.android.api.JPushInterface;
+import java.io.IOException;
+
 import timber.log.Timber;
 
 /**
@@ -27,8 +29,8 @@ public class App extends Application {
         //讯飞语音
         SpeechUtility.createUtility(this, SpeechConstant.APPID + "=56a6efef");
         //极光推送
-        JPushInterface.setDebugMode(true);
-        JPushInterface.init(this);
+//        JPushInterface.setDebugMode(true);
+//        JPushInterface.init(this);
         //初始化 log
         Timber.plant(new Timber.DebugTree());
         //activeAndroid数据库
@@ -42,13 +44,13 @@ public class App extends Application {
         XPGWifiSDK.sharedInstance().startWithAppID(this, Constant.SettingSdkCons.APP_ID);
         XPGWifiSDK.sharedInstance().setLogLevel(Constant.SettingSdkCons.LOG_LEVEL,
                 Constant.SettingSdkCons.LOG_FILE_NAME, Constant.isDebug);
-//        try {
-//            //复制assert文件夹中的json文件到设备安装目录。json文件是解析数据点必备的文件
-//            AssertsUtils.copyAllAssertToCacheFolder(this.getApplicationContext());
-//        } catch (IOException e) {
-//            Timber.e("复制出错");
-//            e.printStackTrace();
-//        }
+        try {
+            //复制assert文件夹中的json文件到设备安装目录。json文件是解析数据点必备的文件
+            AssertsUtils.copyAllAssertToCacheFolder(this.getApplicationContext());
+        } catch (IOException e) {
+            Timber.e("复制出错");
+            e.printStackTrace();
+        }
         //leancloud
         AVOSCloud.initialize(this, "w0nIsCHtpfX5cxQbfiqvnVuz-gzGzoHsz", "SbGChPAMSHouaRtkV8OO8oVk");
         //初始化sd卡文件路径
