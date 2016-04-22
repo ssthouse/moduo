a10db60ea880f3b0583788f43f0ab335860ca4fd
SunsetKnight <462271850@qq.com>
2016-04-22 15:53:21 +0800

    优化文件工具类

--- app/src/main/java/com/mingko/moduo/control/util/FileUtil.java
+++ app/src/main/java/com/mingko/moduo/control/util/FileUtil.java
@@ -12,6 +12,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import java.util.UUID;
 
 /**
  * 文件帮助类{
@@ -23,7 +25,7 @@ import java.util.Date;
 public class FileUtil {
 
     /**
-     * 程序在SD卡的跟路径
+     * 程序在SD卡的根路径
      */
     public static final String MODUO_SDCARD_FOLDER_PATH = SDCardUtil.getSDCardPath() + "Moduo" + File.separator;
 
@@ -42,40 +44,43 @@ public class FileUtil {
      */
     public static void initModuoFolder() {
         //二维码路径
-        File qrCodeFile = new File(MODUO_QRCODE_PATH);
-        if (!qrCodeFile.exists()) {
-            qrCodeFile.mkdirs();
-        }
-        //视频通话的  截图、录像 路径
-        File picFile = new File(MODUO_VIDEO_PIC_PATH);
-        if (!picFile.exists()) {
-            picFile.mkdirs();
-        }
-        File videoFile = new File(MODUO_VIDEO_VIDEO_PATH);
-        if (!videoFile.exists()) {
-            videoFile.mkdirs();
-        }
+        File qrCodePath = new File(MODUO_QRCODE_PATH);
+        createDirectory(qrCodePath);
+        //视频通话的  截图路径
+        File picPath = new File(MODUO_VIDEO_PIC_PATH);
+        createDirectory(picPath);
+        //视频通话的  录像路径
+        File videoPath = new File(MODUO_VIDEO_VIDEO_PATH);
+        createDirectory(videoPath);
         //语音通话记录path
-        File talkFile = new File(MODUO_TALK_PATH);
-        if (!talkFile.exists()) {
-            talkFile.mkdirs();
+        File talkPath = new File(MODUO_TALK_PATH);
+        createDirectory(talkPath);
+    }
+
+    /**
+     * 初始化文件夹
+     *
+     * @param dirPath 文件目录
+     */
+    private static void createDirectory(File dirPath){
+        if (!dirPath.exists()) {
+            dirPath.mkdirs();
         }
     }
 
     /**
-     * 保存bitmap到本地:
+     * 保存魔哆二维码到本地:
      * 因为一台设备的分享二维码是不会改变的, 所以did作为唯一识别
      *
-     * @param bitmap
-     * @param did     以设备号为二维码名称
-     * @return 成功返回路径---不成功返回空
+     * @param bitmap 二维码图片
+     * @param did device id 设备唯一识别号
+     * @return 操作成功返回二维码图片路径，否则返回空
      */
     public static String saveBitmap(Bitmap bitmap, String did) {
         String filePath = MODUO_QRCODE_PATH + did + ".png";
         try {
             OutputStream os = new FileOutputStream(filePath);
             bitmap.compress(Bitmap.CompressFormat.PNG, 90, os);
-            //flush
             os.flush();
             os.close();
         } catch (FileNotFoundException e) {
@@ -91,30 +96,25 @@ public class FileUtil {
     /**
      * 是否已生成图片文件
      *
-     * @param picName
-     * @return
+     * @param picName 图片名
+     * @return 图片存在返回true ，否则返回false
      */
     public static boolean hasPicture(String picName) {
         File picFile = new File(MODUO_QRCODE_PATH + picName);
-        if (picFile.exists()) {
-            return true;
-        } else {
-            return false;
-        }
+        return picFile.exists();
     }
-
 
     /**
      * 调用系统分享文件
      *
-     * @param context
-     * @param file
+     * @param context context
+     * @param picFile 需要分享的图片文件
      */
-    public static void sharePicture(Context context, File file) {
+    public static void sharePicture(Context context, File picFile) {
         //使用intent分享照片
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("image/*");
-        Uri uri = Uri.fromFile(file);
+        Uri uri = Uri.fromFile(picFile);
         shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
         //开启分享
         context.startActivity(shareIntent);
@@ -122,14 +122,13 @@ public class FileUtil {
 
     /**
      * 生成新的视频通话截图文件路径
+     * 文件路径：图片路径\截图时间.jpg
      *
-     * @return 新生成的文件路径
+     * @return 新生成的图片文件路径
      */
     public static String generateNewPicFilePath() {
-        Date date = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_hh:mm:ss");
-        String fileName = dateFormat.format(date);
-        return MODUO_VIDEO_PIC_PATH + fileName + ".jpg";
+
+        return MODUO_VIDEO_PIC_PATH + generateUniqueFileName() + ".jpg";
     }
 
     /**
@@ -138,9 +137,22 @@ public class FileUtil {
      * @return 新的录像文件路径
      */
     public static String generateNewVideoFilePath() {
+        return MODUO_VIDEO_VIDEO_PATH + generateUniqueFileName() + ".mp4";
+    }
+
+    /**
+     * 生成唯一文件名
+     * 文件名由 时间+前8位的UUID序列号作为唯一识别
+     * 关于UUID序列号，可继续优化
+     *
+     * @return 唯一识别文件名
+     */
+    private static String generateUniqueFileName(){
+        String fileName ;
         Date date = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_hh:mm:ss");
-        String fileName = dateFormat.format(date);
-        return MODUO_VIDEO_VIDEO_PATH + fileName + ".mp4";
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_hh:mm:ss", Locale.getDefault());
+        fileName = dateFormat.format(date);
+        fileName += UUID.randomUUID().toString().substring(0,8);
+        return fileName;
     }
 }
